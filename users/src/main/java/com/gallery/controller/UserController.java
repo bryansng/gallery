acc4f772f08d7bc31e3e1975850b69f949131697
Braddy <braddy.yeoh@ucdconnect.ie>
2020-11-18 16:00:39 +0000

    Moved logic to service class

---
+++ users/src/main/java/com/gallery/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.gallery.controller;
+
+import com.gallery.core.response.UserResponse;
+import com.gallery.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    public UserController() {
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<UserResponse> createUser(@RequestBody String username) {
+        return userService.createUser(username);
+    }
+
+    @RequestMapping(value = "/{username}", method = RequestMethod.GET)
+    public ResponseEntity<UserResponse> getUser(@PathVariable("username") String username) {
+        System.out.println(username);
+        return userService.getUser(username);
+    }
+
+    @RequestMapping(value = "/{username}", method = RequestMethod.PUT)
+    public ResponseEntity<UserResponse> updateUsername(@PathVariable("username") String currUsername,
+            @RequestBody String newUsername) {
+        return userService.updateUsername(currUsername, newUsername);
+    }
+
+    @RequestMapping(value = "/{username}", method = RequestMethod.DELETE)
+    public ResponseEntity<UserResponse> deleteUser(@PathVariable("username") String username) {
+        return userService.deleteUser(username);
+    }
+}
--- users/src/main/java/com/gallery/service/UserService.java
+++ users/src/main/java/com/gallery/service/UserService.java
@@ -3,6 +3,7 @@ package com.gallery.service;
 import javax.annotation.PostConstruct;
 
 import com.gallery.config.MongoConfig;
+import com.gallery.core.response.UserResponse;
 import com.gallery.model.User;
 import com.gallery.model.UserSequence;
 
@@ -12,28 +13,22 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 
-@RestController
-@RequestMapping(value = "/user")
+@Service
 public class UserService {
+    private MongoTemplate mongoTemplate;
 
     private final String userCollectionName = "user";
     private final String userSequenceCollectionName = "user_sequence";
 
     @Autowired
     private UserSequenceService userSequenceService;
-
     @Autowired
     private MongoConfig mongoConfig;
 
-    private MongoTemplate mongoTemplate;
-
     public UserService() {
+
     }
 
     @PostConstruct
@@ -41,23 +36,59 @@ public class UserService {
         mongoTemplate = mongoConfig.mongoTemplate();
     }
 
-    @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<String> insert(@RequestBody String username) {
+    public ResponseEntity<UserResponse> createUser(String username) {
+        boolean userExists = mongoTemplate.exists(Query.query(Criteria.where("username").is(username)), User.class,
+                userCollectionName);
+
+        if (userExists) {
+            return new ResponseEntity<>(new UserResponse("Username exists", null), HttpStatus.BAD_REQUEST);
+        }
+
         Long id = userSequenceService.getNext();
         User user = new User(id, username);
 
-        mongoTemplate.save(user, userCollectionName);
+        mongoTemplate.insert(user, userCollectionName);
         mongoTemplate.insert(new UserSequence(id), userSequenceCollectionName);
 
-        return new ResponseEntity<>("User " + username + " created", HttpStatus.CREATED);
+        return new ResponseEntity<>(new UserResponse("User created", user), HttpStatus.CREATED);
     }
 
-    @RequestMapping(method = RequestMethod.PUT)
-    public ResponseEntity<String> update(@RequestParam long id, @RequestBody String username) {
-        User user = mongoTemplate.findOne(Query.query(Criteria.where("id").is(id)), User.class);
-        user.setUsername(username);
+    public ResponseEntity<UserResponse> getUser(String username) {
+        User user = mongoTemplate.findOne(Query.query(Criteria.where("username").is(username)), User.class,
+                userCollectionName);
+
+        if (user == null) {
+            return new ResponseEntity<>(new UserResponse("Username does not exist", null), HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(new UserResponse("Retrieved user", user), HttpStatus.OK);
+    }
+
+    public ResponseEntity<UserResponse> updateUsername(String currUsername, String newUsername) {
+        User user = mongoTemplate.findOne(Query.query(Criteria.where("username").is(currUsername)), User.class,
+                userCollectionName);
+
+        if (user == null) {
+            return new ResponseEntity<>(new UserResponse("Username does not exist", null), HttpStatus.NOT_FOUND);
+        }
+
+        user.setUsername(newUsername);
         mongoTemplate.save(user, userCollectionName);
 
-        return new ResponseEntity<>("User " + username + " updated", HttpStatus.OK);
+        return new ResponseEntity<>(
+                new UserResponse("Username updated from " + currUsername + " to " + newUsername, user), HttpStatus.OK);
+    }
+
+    public ResponseEntity<UserResponse> deleteUser(String username) {
+        User user = mongoTemplate.findOne(Query.query(Criteria.where("username").is(username)), User.class,
+                userCollectionName);
+
+        if (user == null) {
+            return new ResponseEntity<>(new UserResponse("Username does not exist", null), HttpStatus.NOT_FOUND);
+        }
+
+        mongoTemplate.remove(user, userCollectionName);
+
+        return new ResponseEntity<>(new UserResponse("User deleted", user), HttpStatus.OK);
     }
 }
