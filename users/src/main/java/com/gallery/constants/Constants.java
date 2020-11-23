70910de7134120be4aca9c1cb043788a959d4070
Braddy <braddy.yeoh@ucdconnect.ie>
2020-11-23 17:10:50 +0000

    Added constants interface to remove duplicates

---
+++ users/src/main/java/com/gallery/constants/Constants.java
@@ -0,0 +1,6 @@
+package com.gallery.constants;
+
+public interface Constants {
+    public static final String USER_COLLECTION = "user";
+    public static final String USER_SEQUENCE_COLLECTION = "user_sequence";
+}
