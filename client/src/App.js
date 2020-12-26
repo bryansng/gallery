0e4ccacd258ba548c05edb6857cbc2a48bca0fb8
Braddy <braddy.yeoh@ucdconnect.ie>
2020-12-26 15:55:55 +0000

    Initialised client and added examples

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Profile from "./components/Profile/Profile.js";
+
+function App() {
+  return (
+    <div>
+      Hello, this is the root level
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
