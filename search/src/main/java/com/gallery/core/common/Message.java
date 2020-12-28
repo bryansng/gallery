7f9befe99831d696e50d25189a258af058e9524f
Braddy <braddy.yeoh@ucdconnect.ie>
2020-12-28 12:48:03 +0000

    Added LocalDate and LocalTime

--- search/src/main/java/com/gallery/core/common/Message.java
+++ search/src/main/java/com/gallery/core/common/Message.java
@@ -1,14 +1,36 @@
 package com.gallery.core.common;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Message {
     private String msg;
     private LocalDateTime localDateTime;
+    private LocalDate date;
+    private LocalTime time;
 
     public Message(String msg) {
         this.msg = msg;
         this.setLocalDateTime(LocalDateTime.now());
+        this.setDate(localDateTime.toLocalDate());
+        this.setTime(localDateTime.toLocalTime());
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
     }
 
     public LocalDateTime getLocalDateTime() {
