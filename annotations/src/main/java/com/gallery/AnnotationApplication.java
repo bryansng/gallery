0954bfe75389db567e50e04a2e5e74595edd9948
Bryan Sng <hong.sng@ucdconnect.ie>
2020-12-28 13:19:42 +0000

    fixed imports/dependency issue with annotations

--- annotations/src/main/java/com/gallery/AnnotationApplication.java
+++ annotations/src/main/java/com/gallery/AnnotationApplication.java
@@ -1,17 +1,21 @@
 package com.gallery;
 
+import java.util.Properties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @EnableMongoAuditing
 @EnableMongoRepositories({ "com.gallery.*" })
 @SpringBootApplication
 @EnableDiscoveryClient
 public class AnnotationApplication {
-  public static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication application = new SpringApplication(AnnotationApplication.class);
 
 		Dotenv dotenv = Dotenv.configure().directory("../.env").ignoreIfMalformed().ignoreIfMissing().load();
@@ -24,5 +28,5 @@ public class AnnotationApplication {
 		application.setDefaultProperties(properties);
 
 		application.run(args);
-  }
+	}
 }
