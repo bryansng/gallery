4e4bae3534f6ac75231a11eea46a457369f52f27
Braddy <braddy.yeoh@ucdconnect.ie>
2020-12-29 21:31:43 +0000

    Search now displays correct data

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -39,7 +39,35 @@ function Search({ searchEndpoint }) {
     <Container className="mt3">
       <Row>
         <Col className="tc">
-          <Headers>Images</Headers>
+          <Headers>Images by Title</Headers>
+        </Col>
+      </Row>
+      <Row className="mt3">
+        {isLoading
+          ? ""
+          : data.imageResponse.imagesByTitle.searchHits.map((item, key) => {
+              return (
+                <Col className="tc pointer" key={key}>
+                  <ImageOverlay
+                    className="mw5"
+                    src={Dexter}
+                    description={
+                      <Hover>
+                        <HoverContent>
+                          <p className="f3">{item.content.title}</p>
+                        </HoverContent>
+                        <HoverContent>{item.content.description}</HoverContent>
+                        {/* <HoverContent>User</HoverContent> */}
+                      </Hover>
+                    }
+                  />
+                </Col>
+              );
+            })}
+      </Row>
+      <Row>
+        <Col className="tc">
+          <Headers>Images by Description</Headers>
         </Col>
       </Row>
       <Row className="mt3">
@@ -54,9 +82,13 @@ function Search({ searchEndpoint }) {
                       src={Dexter}
                       description={
                         <Hover>
-                          <HoverContent>Title</HoverContent>
-                          <HoverContent>Description</HoverContent>
-                          <HoverContent>User</HoverContent>
+                          <HoverContent>
+                            <p className="f3">{item.content.title}</p>
+                          </HoverContent>
+                          <HoverContent>
+                            {item.content.description}
+                          </HoverContent>
+                          {/* <HoverContent>User</HoverContent> */}
                         </Hover>
                       }
                     />
@@ -64,27 +96,6 @@ function Search({ searchEndpoint }) {
                 );
               }
             )}
-      </Row>
-      <Row className="mt3">
-        {isLoading
-          ? ""
-          : data.imageResponse.imagesByTitle.searchHits.map((item, key) => {
-              return (
-                <Col className="tc pointer" key={key}>
-                  <ImageOverlay
-                    className="mw5"
-                    src={Dexter}
-                    description={
-                      <Hover>
-                        <HoverContent>Title</HoverContent>
-                        <HoverContent>Description</HoverContent>
-                        <HoverContent>User</HoverContent>
-                      </Hover>
-                    }
-                  />
-                </Col>
-              );
-            })}
       </Row>
       <Row>
         <Col className="tc mt5">
@@ -99,7 +110,7 @@ function Search({ searchEndpoint }) {
                 <Col className="tc" key={key}>
                   <UserContainer>
                     <FaUser size={100}></FaUser>
-                    <Username>Dexter</Username>
+                    <Username>{item.content.username}</Username>
                   </UserContainer>
 
                   {/* <Card>
