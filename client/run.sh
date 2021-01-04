2c7f9c7d81531c71943a92503791f43f561395de
Bryan Sng <hong.sng@ucdconnect.ie>
2021-01-04 02:31:28 +0000

    dockerized react-app into nginx instance

---
+++ client/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$1" == "build" ]; then
+  docker build -t client .
+	docker run -it -p 80:80 client:latest
+elif [ "$1" == "spring" ]; then
+  npm start
+else
+  docker run -it -p 80:80 client:latest
+fi
